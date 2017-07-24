dc38ba2d15238cf2dfc1251a8d06d33c6c82185c
clarkmcadoo <clark.mcadoo@gmail.com>
2017-07-24 12:19:01 -0500

    init

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+class BaseLayout extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+          {this.props.children}
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default BaseLayout;
